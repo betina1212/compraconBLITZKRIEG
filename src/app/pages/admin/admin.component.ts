f7d2bab1352e6da98d55a063da67bbc0c58e7cc9
DilanVargas666 <dilancito.6969@gmail.com>
2019-11-07 19:56:29 -0400

    modulo administrador y validacion dedatos

---
+++ src/app/pages/admin/admin.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/user/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  public viewVendedores() {
+    this.router.navigate(['/admin/vendedores']);
+  }
+
+  public viewClintes() {
+    this.router.navigate(['/admin/clientes']);
+  }
+
+}
